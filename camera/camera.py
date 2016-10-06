1e94111c73a991b2d1364a8ac29de7b6199ed142
Nuria <nuria.oyaga@gmail.com>
2016-10-06 17:47:59 +0200

    Creado archivos para funcionamiento de la camara

---
+++ camera/camera.py
@@ -0,0 +1,52 @@
+import sys, traceback, Ice
+import jderobot
+import numpy as np
+import threading
+import cv2
+import Image
+
+class Camera():
+
+    def __init__(self):
+
+        self.lock = threading.Lock()
+
+        try:
+            ic = Ice.initialize()
+            obj = ic.stringToProxy('cameraA:default -h localhost -p 9999')
+            self.camera = jderobot.CameraPrx.checkedCast(obj)
+            if self.camera:
+                self.image = self.camera.getImageData("RGB8")
+                self.height= self.image.description.height
+                self.width = self.image.description.width
+            else:
+                print 'Interface camera not connected'
+
+        except:
+            traceback.print_exc()
+            exit()
+            status = 1
+
+    def getImage(self):
+        if self.camera:
+            self.lock.acquire()
+            image = np.zeros((self.height, self.width, 3), np.uint8)
+            image = np.frombuffer(self.image.pixelData, dtype=np.uint8)
+            image.shape = self.height, self.width, 3
+            imageTrans = self.trasformImage(image)
+            images = [image,imageTrans]
+            self.lock.release()
+        return images
+
+    def update(self):
+        if self.camera:
+            self.lock.acquire()
+            self.image = self.camera.getImageData("RGB8")
+            self.height= self.image.description.height
+            self.width = self.image.description.width
+            self.lock.release()
+
+    def trasformImage(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #resize= cv2.resize(gray,(28,28))
+        return gray
